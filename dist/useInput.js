872fa3108d2f98ace73687c8cd22332e286762ba
AnonymousCoder2020 <virtual.creeper.ver1.3.2@gmail.com>
2024-05-03 21:04:40 +0900

    FIx useInput.

--- dist/useInput.js
+++ dist/useInput.js
@@ -1,7 +1,7 @@
 import useAddEventListener from './useAddEventListener';
 export default (r, initialState) => {
     const [core, setCore] = r.useState(initialState ?? '');
-    const { ref, refEl } = useAddEventListener(r, 'change', ({ target }) => setCore(target.value));
+    const { ref, refEl } = useAddEventListener(r, 'input', ({ target }) => setCore(target.value));
     const setState = r.useCallback((newState) => {
         const inputEl = refEl.current;
         if (!inputEl)
--- src/useInput.ts
+++ src/useInput.ts
@@ -2,7 +2,7 @@ import type React from 'react'
 import useAddEventListener from './useAddEventListener'
 export default <T extends HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>(r: typeof React, initialState: string) => {
   const [core, setCore] = r.useState(initialState ?? '')
-  const { ref, refEl } = useAddEventListener<'change', T>(r, 'change', ({ target }) => setCore(target.value))
+  const { ref, refEl } = useAddEventListener<'input', T>(r, 'input', ({ target }) => setCore(target.value))
   const setState = r.useCallback((newState: string) => {
     const inputEl = refEl.current
     if (!inputEl) return
